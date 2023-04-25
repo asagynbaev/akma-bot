381a7095084d051a05da5ce5778974c484a60d0c
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2023-04-25 16:13:37 +0600

    stash

---
+++ TelegramBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace MindMate
+{
+	public class TelegramBot
+	{
+        private static TelegramBotClient client { get; set; }
+
+        public static TelegramBotClient GetTelegramBot()
+        {
+            if (client != null)
+            {
+                return client;
+            }
+            client = new TelegramBotClient("6001984916:AAEGRpus9lLtFyAUnthmsECTK4wiRmB8qmY");
+            return client;
+        }
+    }
+}
+
