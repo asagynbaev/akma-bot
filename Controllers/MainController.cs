dfcd91899fb9ce0a1d2c526d186992004d67e1b1
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2023-04-25 15:48:04 +0600

    First messages of bot

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,22 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
 
-namespace MindMate.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class MainController : ControllerBase
+namespace MindMate.Controllers
 {
-    private readonly ILogger<MainController> _logger;
-
-    public MainController(ILogger<MainController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
     {
-        _logger = logger;
-    }
+        private readonly ILogger<MainController> _logger;
 
-    [HttpGet(Name = "ping")]
-    public string Get()
-    {
-        return "Hello from MindMate";
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Post(Update update) //Update receiver method
+        {
+            if (update != null)
+            {
+                if (update.Message != null)
+                {
+                    _logger.LogInformation(update.Message.Text);
+                    Console.WriteLine(update.Message.Text);
+                }
+                else
+                    _logger.LogInformation("update.Message is null");
+            }
+            else
+                _logger.LogInformation("update is null");
+        }
+        [HttpGet]
+        public string Get()
+        {
+            //Init point
+            return "Telegram bot was started";
+        }
     }
 }
+
+
 
